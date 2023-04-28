4f173751c82aa21bedfa490a1883bf6eef568b0d
ChrisYMG <cmejiagalindo@yahoo.com>
2023-04-27 23:35:04 -0600

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,55 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const useTodo = (initialState = []) => {
+  // [state, action] = useReducer(reducer, initialSatte, callback)
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  // "todos" que es el state contiene el "initialState"
+  // "dispatch" contiene las acciones que quiero que haga el "todoReducer"
+  // quedaría todoReducer(todos, dispatch)
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter((todo) => !todo.done).length;
+
+  return {
+    ...todos,
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
